3fb06d38d9de8fc0bd2484755e488bc26111549e
Wing Fung Wu <44103783+WingFungWu@users.noreply.github.com>
2022-10-30 00:56:27 -0700

    Multi-regression made

--- display_datasets.py
+++ display_datasets.py
@@ -1,5 +1,7 @@
 # %%
 import pandas as pd
+from sklearn import linear_model
+import statsmodels.api as sm
 
 # %%
 def show_milk_breastcancer_graph(df, year):
@@ -23,11 +25,28 @@ def show_soy_breastcancer_graph(df, year):
         title="Breast Cancer Rates in {}".format(year)
     )
 
-#%% 
-df = pd.read_csv("updated_datasets/merged_dataset.csv")
 # %%
-show_milk_breastcancer_graph(df, 2015)
+def show_mutton_and_goat_graph(df, year):
+    df.loc[
+        df["Year"] == year
+    ].plot(
+        kind="scatter",
+        x="Mutton and Goat Consumption",
+        y="Life Expectancy",
+        title="Life Expectancy in {}".format(year)
+    )
+
+# %% 
+df = pd.read_csv("updated_datasets/merged_dataset.csv")
 
 # %%
-show_soy_breastcancer_graph(df, 2015)
+x = df[["Mutton and Goat Consumption", "Beef and Buffallo Consumption", "Pigmeat Consumption", "Poultry Consumption", "Other Meats Consumption"]]
+y = df["Life Expectancy"]
+regr = linear_model.LinearRegression()
+regr.fit(x, y)
+x = sm.add_constant(x)
+model = sm.OLS(y, x).fit()
+print_model = model.summary()
+print(print_model)
+
 # %%
--- merge_datasets.py
+++ merge_datasets.py
@@ -2,22 +2,38 @@ import pandas as pd
 from pathlib import Path  
 
 def main():
-    df_cancer, df_milk, df_soy = read_original_datasets()
-    df_cancer, df_milk, df_soy = cleanup_original_datasets(df_cancer, df_milk, df_soy)
+    df_cancer, df_milk, df_soy, df_life_expectancy, df_meat_consumption = read_original_datasets()
+    df_cancer, df_milk, df_soy, df_life_expectancy, df_meat_consumption = cleanup_original_datasets(df_cancer, df_milk, df_soy, df_life_expectancy, df_meat_consumption)
     df_merged = merge_datasets(df_cancer, df_milk, df_soy)
-    save_merged_to_csv(df_merged)
+    df_new_merged = merge_new_datasets(df_life_expectancy, df_meat_consumption)
+    save_merged_to_csv(df_new_merged)
+    "save_merged_to_csv(df_merged)"
 
 def read_original_datasets():
     df_cancer = pd.read_csv("original_datasets/share-of-population-with-cancer-types.csv").fillna("0")
     df_milk = pd.read_csv("original_datasets/per-capita-milk-consumption.csv").fillna("0")
     df_soy = pd.read_csv("original_datasets/soybean-production-and-use.csv").fillna("0")
-    return df_cancer, df_milk, df_soy
 
-def cleanup_original_datasets(df_cancer, df_milk, df_soy):
+    df_life_expectancy = pd.read_csv("original_datasets/life-expectancy.csv").fillna("0")
+    df_meat_consumption = pd.read_csv("original_datasets/per-capita-meat-consumption-by-type-kilograms-per-year.csv").fillna("0")
+    return df_cancer, df_milk, df_soy, df_life_expectancy, df_meat_consumption
+
+def cleanup_original_datasets(df_cancer, df_milk, df_soy, df_life_expectancy, df_meat_consumption):
     # Rename entries
     df_milk["Entity"] = df_milk["Entity"].replace("Sudan (former)", "Sudan")
-
+    df_meat_consumption["Entity"] = df_meat_consumption["Entity"].replace("Sudan (former)", "Sudan")
     # Rename and prune columns
+    df_life_expectancy = df_life_expectancy.loc[df_life_expectancy["Entity"] != "World"].rename(columns={
+        "Life expectancy": "Life Expectancy"
+    })[["Entity", "Year", "Life Expectancy"]]
+    df_meat_consumption = df_meat_consumption.loc[df_meat_consumption["Entity"] != "World"].rename(columns={
+        "Meat, Other, Food supply quantity (kg/capita/yr) (FAO, 2020)": "Other Meats Consumption",
+        "Mutton & Goat meat food supply quantity (kg/capita/yr) (FAO, 2020)": "Mutton and Goat Consumption",
+        "Bovine meat food supply quantity (kg/capita/yr) (FAO, 2020)": "Beef and Buffallo Consumption",
+        "Pigmeat food supply quantity (kg/capita/yr) (FAO, 2020)": "Pigmeat Consumption",
+        "Poultry meat food supply quantity (kg/capita/yr) (FAO, 2020)": "Poultry Consumption"
+    })[["Entity", "Year", "Mutton and Goat Consumption", "Beef and Buffallo Consumption", "Pigmeat Consumption",
+    "Poultry Consumption", "Other Meats Consumption"]]
     df_cancer = df_cancer.loc[df_cancer["Entity"] != "World"].rename(columns={
         "Prevalence - Breast cancer - Sex: Both - Age: Age-standardized (Percent)": "Breast Cancer Rates",
     })[["Entity", "Year", "Breast Cancer Rates"]]
@@ -27,7 +43,7 @@ def cleanup_original_datasets(df_cancer, df_milk, df_soy):
     df_soy = df_soy.loc[df_soy["Entity"] != "World"].rename(columns={
         "Soybeans | 00002555 || Food | 005142 || tonnes": "Soy Consumption"
     })[["Entity", "Year", "Soy Consumption"]]
-    return df_cancer, df_milk, df_soy
+    return df_cancer, df_milk, df_soy, df_life_expectancy, df_meat_consumption
 
 def merge_datasets(df_cancer, df_milk, df_soy):
     return df_cancer[[
@@ -37,6 +53,14 @@ def merge_datasets(df_cancer, df_milk, df_soy):
         on=["Entity", "Year"]
     ).merge(
         df_soy,
+        on=["Entity", "Year"]
+    )
+
+def merge_new_datasets(df_life_expectancy, df_meat_consumption):
+    return df_life_expectancy[[
+        "Entity", "Year", "Life Expectancy"
+    ]].merge(
+        df_meat_consumption,
         on=["Entity", "Year"]
     )
 
